5e8d5c4fa9d3534b618c70bdfdccce08500075af
paulspam <paulspam@ukr.net>
2021-03-01 14:18:17 +0200

    9. Spring MVC Select form

--- src/main/java/com/zaurtregulov/spring/mvc/Employee.java
+++ src/main/java/com/zaurtregulov/spring/mvc/Employee.java
@@ -1,12 +1,20 @@
 package com.zaurtregulov.spring.mvc;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Employee {
     private String name;
     private String surname;
     private int salary;
     private String department;
+    private Map<String, String> departmens;
 
     public Employee() {
+        departmens = new HashMap<>();
+        departmens.put("IT", "Information Technology");
+        departmens.put("HR", "Human Resources");
+        departmens.put("Sales", "Sales");
     }
 
     public String getName() {
@@ -39,6 +47,14 @@ public class Employee {
 
     public void setDepartment(String department) {
         this.department = department;
+    }
+
+    public Map<String, String> getDepartmens() {
+        return departmens;
+    }
+
+    public void setDepartmens(Map<String, String> departmens) {
+        this.departmens = departmens;
     }
 
     @Override
--- src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
+++ src/main/webapp/WEB-INF/view/ask-emp-details-view.jsp
@@ -24,6 +24,16 @@
     <br><br>
     Salary <form:input path="salary"/>
     <br><br>
+    Department <form:select path="department">
+    <form:options items="${employee.departmens}"/>
+
+<%--    <form:option value="Information Technology" label="IT"/>--%>
+<%--    <form:option value="Human Resources" label="HR"/>--%>
+<%--    <form:option value="Sales" label="Sales"/>--%>
+
+
+    </form:select>
+    <br><br>
     <input type="submit" value="OK">
 </form:form>
 
